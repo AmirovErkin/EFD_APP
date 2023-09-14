87e4b0a9a6bde59939e9ac444f7b07d99ca77e0c
AmirovErkin <125683368+AmirovErkin@users.noreply.github.com>
2023-09-14 14:40:32 +0500

    Delete efd directory

--- efd/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    full_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)

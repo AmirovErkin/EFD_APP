44d9997cdcc5d3c3e38d9d84b0e73cc8985b8853
AmirovErkin <erkinamirov995@gmail.com>
2023-09-14 14:38:20 +0500

    Update project

---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
